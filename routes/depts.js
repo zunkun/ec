85312a8c49c8be7b6fabfec54b6c12022a93aa42
zunkun <liuzunkun@gmail.com>
2019-04-25 16:45:09 +0800

    部门api管理修改

--- routes/depts.js
+++ routes/depts.js
@@ -11,7 +11,7 @@ router.prefix('/api/depts');
 /**
  * @api {get} /api/depts/?year=:year 获取部门列表
  * @apiName get-dept-lists
- * @apiDescription 获取部门列表,即需要做费控的部门列表，此列表只是钉钉部门的一级部门 【需要登录】
+ * @apiDescription 获取部门列表 【需要登录】
  * @apiGroup 部门接口
  * @apiParam {Number} year 年份 默认当年
  * @apiSuccess {Number} errcoce 正确时为0，错误时为错误代码
@@ -27,9 +27,8 @@ router.prefix('/api/depts');
  */
 router.get('/', async (ctx, next) => {
 	let year = Number(ctx.query.year) || (new Date()).getFullYear();
-	let depts = await Depts.find({ year });
+	let depts = await Depts.find({ year, corpId: config.corpId });
 	let data = { year, depts: [] };
-
 	for (let dept of depts) {
 		data.depts.push({
 			deptId: dept.deptId,
@@ -71,7 +70,7 @@ router.get('/group', async (ctx, next) => {
 			name: deptGroup.name,
 			depts: []
 		};
-		let depts = await Depts.find({ 'group.code': deptGroup.code }) || [];
+		let depts = await Depts.find({ 'group.code': deptGroup.code, year }) || [];
 		for (let dept of depts) {
 			group.depts.push({
 				deptId: dept.deptId,
@@ -85,14 +84,14 @@ router.get('/group', async (ctx, next) => {
 });
 
 /**
- * @api {post} /api/depts/group 生成部门和预算体对应关系
+ * @api {post} /api/depts/groupmap 生成部门和预算体对应关系
  * @apiName set-dept-group
  * @apiDescription 生成部门和预算体对应关系 【需要登录】
  * @apiGroup 部门接口
  * @apiParam {Number} year 年份 默认当年
  * @apiParam {Number} deptId 部门deptId
  * @apiParam {String} code 预算体编号
- * @apiParam {Boolean} setChild 是否设置子部门，true 时将子部门也对应父部门的预算体
+ * @apiParam {Boolean} notChild 是否不应用到子部门，默认false, true 时不会设置子部门对应父部门的预算体
  * @apiSuccess {Number} errcoce 正确时为0，错误时为错误代码
  * @apiSuccess {String} errmsg 错误提示，正确返回为空字符
  * @apiSuccess {Boolean} success 是否正确返回，true 正确, false错误
@@ -101,8 +100,8 @@ router.get('/group', async (ctx, next) => {
  * @apiSuccess {Number} data.deptId 部门deptId
  * @apiSuccess {String} data.code 预算体code
  */
-router.post('/group', async (ctx, next) => {
-	let { deptId, code, year, setChild } = ctx.request.body;
+router.post('/groupmap', async (ctx, next) => {
+	let { deptId, code, year, notChild } = ctx.request.body;
 	year = Number(year) || new Date().getFullYear();
 	deptId = Number(deptId);
 	if (!deptId || !code) {
@@ -121,11 +120,11 @@ router.post('/group', async (ctx, next) => {
 		ctx.body = ServiceResult.getFail('系统没有找到部门信息', 404);
 		return;
 	}
-	let conditions = { deptId };
-	if (setChild) {
+	let conditions = { deptId, year };
+	if (!notChild) {
 		conditions.deptPath = deptId;
 	}
-
+	console.log(conditions, { group: { code, name: deptGroup.name } });
 	await Depts.updateOne(conditions, { group: { code, name: deptGroup.name } });
 
 	ctx.body = ServiceResult.getSuccess(ctx.request.body);
